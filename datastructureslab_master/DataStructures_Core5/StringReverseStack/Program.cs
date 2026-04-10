ac0219a277d0d3cdcb7cde29613a1cff14cd2419
Joseph Paul Teague <graveytea@gmail.com>
2026-04-10 16:21:20 -0700

    Initial Upload with problem 1 completed...
    I forgot to do this before editing lol

---
+++ datastructureslab_master/DataStructures_Core5/StringReverseStack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringReverseStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deliver instructions and store input in a variable
+            Console.WriteLine("Olleh! Everybody loves talking backwards! Let's get some practice in...");
+            Console.WriteLine("Enter some text, and I'll reverse it for you so you can work on your backwards talking skills: ");
+            string input = Console.ReadLine();
+
+
+            // Turn the input into a stack with a foreach
+            Stack<char> chars = new Stack<char>();
+            foreach (char c in input)
+            {
+                chars.Push((char)c);
+            }
+
+
+            // Use the Pop method to store the backwards string in a variable and output to user
+            string backwards = "";
+            while (chars.Count > 0)
+            {
+                backwards += chars.Pop();
+            }
+
+            Console.WriteLine("In backwards talk, that would be...  " + backwards);
+        }
+
+    }
+}
